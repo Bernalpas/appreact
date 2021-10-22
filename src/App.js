e102554151148c157252e091a2c7637ba69d3979
Berni <bernalpas@gmail.com>
2021-10-22 20:42:53 -0300

    Aplicación terminada y lista para el deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Counter } from './Componentes/Contador';
+import { Navegacion } from './Componentes/Navegacion';
+import { Footer } from './Componentes/Footer';
+import { Productos } from './Componentes/Productos';
 
-function App() {
+
+class App extends Component {
+  render(){
   return (
     <div className="App">
+
+      <nav>
+        <Navegacion/>
+      </nav>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
+
+      <main>
+        <p>
+          <h1>Me sumo al Componente App</h1>
+        </p>
+        <Counter></Counter>
+        <Productos/>
+      </main>
+
+      <Footer/>  
+
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/Componentes/Contador.jsx
@@ -0,0 +1,41 @@
+
+import { Component } from "react";
+
+
+export class Counter extends Component{
+
+    constructor(){
+        super();
+        this.state = {
+            contador: 1
+        }
+    }
+
+    handleIncrement = () => {
+        this.setState(state => ({ contador: state.contador + 1}))
+    }
+
+    render(){
+        return(
+            <>
+                <div>
+                    <h1>
+                        {this.state.contador}
+                    </h1>
+                </div>                
+                <div>
+                    <button onClick={this.handleIncrement} type="button" class="btn btn-success"> Incrementar el Contador </button>
+                        
+                </div>
+            </>
+        );
+    }
+
+}
+
+/* export default Counter; */
+
+
+
+
+
